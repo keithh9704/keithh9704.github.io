d0821a5c2f6db4355dd378f02c57eeef447d67f8
keithh9704 <146886714+keithh9704@users.noreply.github.com>
2023-10-03 16:19:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Keith Hau"; // update the part BETWEEN the "quotes"
+let userName = "keithh9704"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
